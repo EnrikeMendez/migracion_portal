69cba13486a25995d71fd31c529c2e66452d8b6b
EnrikeMendez <logis.enrique.mendez@gmail.com>
2024-03-22 16:52:00 -0600

    Se sube carpeta de documentacion y se crean directorios para cada seccion del proyecto

---
+++ 01-CodigoFuente/wsDistribucion/Class/ClsRespuesta.cs
@@ -0,0 +1,7 @@
+﻿public class Respuesta
+{
+    public int Codigo { get; set; }
+    public bool Ok { get; set; }
+    public string Mensaje { get; set; }
+    public object Objeto { get; set; }
+}
---
+++ 01-CodigoFuente/wsDistribucion/Controllers/DocumentaLTLController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+[ApiController]
+[Route("[Controller]")]
+public class DocumentaLTLController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpPost("documentarnui")]
+    public WEB_LTL DocumentarNUI(WEB_LTL Info)
+    {
+        WEB_LTL wel = new WEB_LTL();
+        Respuesta resp = new Respuesta();
+
+        resp = wel.ValidaInfo(Info);
+
+        if (resp.Ok == true)
+        {
+            resp = wel.DocumentaTalon(Info);
+
+            if (resp.Ok == true)
+            {
+                Info = (WEB_LTL)resp.Objeto;
+            }
+        }
+
+        return Info;
+    }
+}
