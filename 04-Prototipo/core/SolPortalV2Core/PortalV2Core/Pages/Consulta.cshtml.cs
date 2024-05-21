4d95d89388a9e4288f1d8b3260e1530e5c997da9
cesarpiscil <cesarpiscil68@gmail.com>
2024-05-21 14:19:24 -0600

    Se sube aplicacion web

---
+++ 04-Prototipo/core/SolPortalV2Core/PortalV2Core/Pages/Consulta.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PortalV2Core.Pages
+{
+    public class ConsultaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
