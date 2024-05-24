0232dba4169e6d9048cf758c32720293175c96a1
cesarpiscil <cesarpiscil68@gmail.com>
2024-05-24 11:18:59 -0600

    se boostrapea el proyecto

--- 04-Prototipo/webdistribucion/obj/Debug/net8.0/webdistribucion.AssemblyInfo.cs
+++ 04-Prototipo/webdistribucion/obj/Debug/net8.0/webdistribucion.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webdistribucion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3adb232ea0c2fd5b17e70eb93ddb3773801d2f4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f1b018673e4276736a822f2b599cfc899774182")]
 [assembly: System.Reflection.AssemblyProductAttribute("webdistribucion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webdistribucion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
